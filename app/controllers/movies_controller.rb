26abebc3ed32d2c82777616582628c39ee1b0916
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-12 01:17:51 +0300

    Rendering summary of full collection of movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,25 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+
+  # Adding summary action to our controller. 
+  # we specifying  that we want to use our new serializer to 
+  # render the requested information:
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  #Render summary of full collection of movies
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
   private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
   end
+
 end
