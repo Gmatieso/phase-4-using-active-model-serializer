13ab5cb8ab2bbc4a16628a46c0c10ed2c2184d7a
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-12 00:23:43 +0300

    Customizing our json

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id
+  #customizing our json by simplying providing the list of  attributes that we want to be include 
+  attributes :id, :title, :year, :length, :director, :description, 
+  :poster_url, :category, :discount, :female_director
 end
