7b601714280cd8f28970c1daca640f3d5922de01
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-12 00:59:19 +0300

    Created an Instance method Summary

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,4 +2,8 @@ class MovieSerializer < ActiveModel::Serializer
   #customizing our json by simplying providing the list of  attributes that we want to be include 
   attributes :id, :title, :year, :length, :director, :description, 
   :poster_url, :category, :discount, :female_director
+  
+  def summary
+    "#{self.object.title} - #{self.object.description[0..49]}..."
+  end
 end
